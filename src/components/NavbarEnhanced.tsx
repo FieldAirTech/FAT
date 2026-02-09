902202c4dc5429d82862a20812ddbaaf5a960c25
FieldAirTech <comercial@fieldairtech.com>
2026-02-09 09:33:41 +0000

    Update NavbarEnhanced.tsx

--- src/components/NavbarEnhanced.tsx
+++ src/components/NavbarEnhanced.tsx
@@ -149,7 +149,7 @@ export default function Navbar() {
             <Link to="/" className="flex items-center group">
               <div className="relative">
                 <img
-                  src="https://i.imgur.com/gq0vsT0.png"
+                  src="https://i.imgur.com/ycV6enj.png"
                   alt="FieldAirTech Logo"
                   className={`w-46 h-12 object-contain transition-all duration-300 ${
                     scrolled ? 'h-24' : 'h-24'
